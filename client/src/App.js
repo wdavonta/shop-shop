9a7287d032136e13f5afb34dfca7a97c5a812de5
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-04-01 23:30:04 -0400

    remove global state from app js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import { StoreProvider } from "./utils/GlobalState";
 import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
@@ -30,7 +29,6 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -41,7 +39,6 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
